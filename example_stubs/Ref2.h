6befe7a76c112d92651d45eb11b8f7884a593d7b
qidasheng <qsf.zzia1@hotmail.com>
2014-10-21 17:24:19 +0800

    删除无用信息

--- example_stubs/Ref2.h
+++
@@ -1,4 +0,0 @@
-/* Stub declarations to make some examples in Ref2 work */
-
-void continue_running_parent(struct event_base *);
-void continue_running_child(struct event_base *);
--- run.sh
+++
@@ -1,7 +0,0 @@
-#valgrind --tool=memcheck --leak-check=full --track-origins=yes  --show-reachable=yes -v ./xlog -c ./xlog_new.conf 
-make clean
-make uninstall
-./configure 
-make
-make install
-
